3f5effabc8229877c908121d898de32297c675f4
shut <118461160+shutjs@users.noreply.github.com>
2023-04-05 20:06:12 +0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  Guild:{
+   ID: ""
+  },
+  Prefix: ".",
+
+  Users: {
+    OWNERS: ["669513018777796638"] 
+  },
+
+  Handlers: {
+    MONGO: ""
+  },
+
+  Client: {
+    TOKEN: "", 
+    ID: "" 
+  },
+
+}
